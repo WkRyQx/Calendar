7cd1cc923d39b9a95325f6fb342be2c16eac7344
Laktusz <153062991+Laktusz@users.noreply.github.com>
2026-02-02 13:08:39 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="regist.css">
+</head>
+<body>
+    <header></header>
+    <div class="registration">
+        <div>
+            <div id="input">
+                <input type="text" placeholder="Email: ">
+                <input type="password" placeholder="Jelszó: ">
+            </div>
+            <button>Bejelentkezés</button>
+        </div>
+        <div class="gombok_login">
+            <button type="button" id="bejGomb2"><a href="login.php">Bejelentkezés</a></button>
+            <button type="button" id="regGomb2"><a href="registration.php">Regisztráció</a></button>
+        </div>
+    </div>
+</body>
+</html>
---
+++ registration.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar</title>
+    <link rel="stylesheet" href="regist.css">
+</head>
+<body>
+    <header></header>
+    <div class="registration">
+        <div>
+            <div id="input">
+                <input type="text" placeholder="Felhasználónév: ">
+                <input type="text" placeholder="Email: ">
+                <input type="password" placeholder="Jelszó: ">
+            </div>
+            <button>Regisztráció</button>
+        </div>
+        <div class="gombok_regist">
+            <button type="button" id="bejGomb"><a href="login.php">Bejelentkezés</a></button>
+            <button type="button" id="regGomb"><a href="registration.php">Regisztráció</a></button>
+        </div>
+    </div>
+</body>
+</html>
