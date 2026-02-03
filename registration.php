77ead0e162fe8e875355ffd3ca89ab7ddbe9f052
Laktusz <153062991+Laktusz@users.noreply.github.com>
2026-02-03 09:13:35 +0100

    Add files via upload

--- registration.php
+++ registration.php
@@ -5,17 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Calendar</title>
     <link rel="stylesheet" href="regist.css">
+    <script src="regist.js" defer></script>
 </head>
 <body>
     <header></header>
     <div class="registration">
         <div>
             <div id="input">
-                <input type="text" placeholder="Felhasználónév: ">
-                <input type="text" placeholder="Email: ">
-                <input type="password" placeholder="Jelszó: ">
+                <input class="input_text" type="text" placeholder="Felhasználónév: ">
+                <input class="input_text" type="text" placeholder="Email: ">
+                <input class="input_text" type="password" placeholder="Jelszó: ">
             </div>
-            <button>Regisztráció</button>
+            <button class="submit_gomb" type="button">Regisztráció</button>
         </div>
         <div class="gombok_regist">
             <button type="button" id="bejGomb"><a href="login.php">Bejelentkezés</a></button>
